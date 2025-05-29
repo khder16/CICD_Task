37fa17f65519cd5fb18334decb6b143fac007b7c
khder16 <khderhabeb16@gmail.com>
2025-05-29 13:34:40 +0300

    Add hello message function in api controller

--- src/controllers/api.controllers.ts
+++ src/controllers/api.controllers.ts
@@ -3,3 +3,7 @@ import { Request, Response, RequestHandler } from 'express';
 export const getApiStatus = (req: Request, res: Response): void => {
     res.status(200).json({ status: 'API is working' });
 };
+
+export const sendHelloMessage = (req: Request, res: Response): void => {
+    res.status(200).json({ status: 'Hello from api' });
+};
