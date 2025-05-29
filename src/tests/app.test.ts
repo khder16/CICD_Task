560078641461c6ada28a74f06e4208a3d5a9842d
khder16 <khderhabeb16@gmail.com>
2025-05-29 13:40:46 +0300

    Add Unit Test for hello function

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -11,3 +11,17 @@ describe('API Integration Tests', () => {
         });
     });
 });
+
+
+// Check Hello function
+
+describe('GET /api/hello', () => {
+    it('should return 200 and "Hello from api" message', async () => {
+        const response = await request(app).get('/api/hello');
+        // Basic response validation
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            status: 'Hello from api'
+        })
+    })
+})
